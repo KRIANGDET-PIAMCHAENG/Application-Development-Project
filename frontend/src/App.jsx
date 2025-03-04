4f61ab4097cae6c16df2f48f5d1b57f675e2441f
IMLV1 <teetat.pit@ku.th>
2025-03-05 00:36:12 +0700

    browserRoute

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,16 @@
-import './App.css'
-import Login from './Login'
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import './App.css';
+import Login from './Login';
 
 function App() {
   return (
-    <div>
-      <Login/>
-    </div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
